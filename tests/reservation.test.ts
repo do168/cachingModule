9ad88bc15e8fe3a05a36578a1c2de309b2f40625
ddan <do1235580@gmail.com>
2021-04-29 15:38:27 +0900

    fix: refactor test code

--- tests/reservation.test.ts
+++ tests/reservation.test.ts
@@ -3,6 +3,7 @@ import { createTestClient } from 'apollo-server-testing';
 import { getPool } from '../src/common/db';
 import { gql } from 'apollo-server-express';
 import { initDB } from '../src/common/createSchema';
+import { execute } from 'graphql';
 
 const app = new App();
 const server = app.buildServer();
@@ -11,9 +12,7 @@ const { query, mutate } = createTestClient(server);
 const GET_RESERVATION = gql`
   query($id: ID!) {
     reservation {
-      reservation(id: $id) {
-        name
-      }
+      reservation(id: $id)
     }
   }
 `;
@@ -24,17 +23,15 @@ beforeAll(async () => {
 
   // 0: THEME, 1: PRODUCT
   await pool.query(`INSERT INTO ReservationTest (name) VALUES ("test")`);
-
-  console.log('test');
 });
 
-// afterAll(async () => {
-//   const pool = getPool();
-//   // cleanup
-//   await pool.query(`DELETE FROM ReservationTest`);
-//   await pool.query(`ALTER TABLE ReservationTest AUTO_INCREMENT = 1`);
-//   await pool.end();
-// })
+afterAll(async () => {
+  const pool = getPool();
+  // cleanup
+  await pool.query(`DELETE FROM ReservationTest`);
+  await pool.query(`ALTER TABLE ReservationTest AUTO_INCREMENT = 1`);
+  await pool.end();
+});
 
 describe('Reservation Queries', () => {
   test('find reservation', async () => {
@@ -44,6 +41,6 @@ describe('Reservation Queries', () => {
         id: 1,
       },
     });
-    expect(res?.data?.reservation?.reservation?.name).toBe('test');
+    expect(res?.data?.reservation?.reservation).toBe('"test"');
   });
 });
